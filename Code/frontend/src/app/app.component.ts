4d16eddae88db4342eb5a75754b540521ef03db0
Fernando Celmer <email@fernandocelmer.com>
2020-11-22 11:46:20 -0300

    OFFY-02 - Criação de um service para acessar nossa api e uma view

--- Code/frontend/src/app/app.component.ts
+++ Code/frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from './api.service';
+import { ShoppingItem } from './shopping-item.interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,3 +10,34 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'frontend';
 }
+
+@Component({
+    selector: 'app-root',
+    template: `
+    <div style="text-align:center">
+      <h1>
+        Lista de compras
+      </h1>
+    </div>
+    <ul>
+      <li *ngFor="let item of items">
+        <h2>{{ item.name }}</h2>
+      </li>
+    </ul>
+    {{ error?.message }}
+    `
+  })
+  export class AppComponent implements OnInit {
+  
+    items: ShoppingItem[];
+    error: any;
+  
+    constructor(private api: ApiService) { }
+  
+    ngOnInit() {
+      this.api.getShoppingItems().subscribe(
+        (items: ShoppingItem[]) => this.items = items,
+        (error: any) => this.error = error
+      );
+    }
+  }
